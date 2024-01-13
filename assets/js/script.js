d2e33e5558a7e7e63681b93621ae99da31879cc7
astro-mat <whoismatwarren@gmail.com>
2024-01-13 22:02:43 +0000

    Add javascript prompt generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,75 @@
+let generateButton = document.getElementById("generate-button");
+let outputFirst = document.getElementById("outputFirst");
+let outputSecond = document.getElementById("outputSecond");
+let outputThird = document.getElementById("outputThird");
 
 
-
+/*
 var firstRandom = getRandomNumber(firstColumn.length - 1);
 var secondRandom = getRandomNumber(secondColumn.length - 1);
 var thirdRandom = getRandomNumber(thirdColumn.length - 1);
+*/
+let num1 = Math.floor(Math.random() * 11);
+let num2 = Math.floor(Math.random() * 11);
+let num3 = Math.floor(Math.random() * 11);
 
 let firstColumn = [
-    "1st string E only", 
-    "2nd string B only",
-    "3rd string G only",
-    "4th string D only",
-    "5th string A only",
-    "6th string E only",
-    "Double stops only",
-    "top 3 strings",
-    "Bottom 3 strings",
-    "no adjacent strings (Always skip if changing strings)",
-    "No string restrictions"];
+    "Use the 1st string (E) only.", 
+    "Use the 2nd string (B) only.",
+    "Use the 3rd string (G) only.",
+    "Use the 4th string (D) only.",
+    "Use the 5th string (A) only.",
+    "Use the 6th string (E) only.",
+    "Use Double stops only.",
+    "Use top 3 strings.",
+    "Use Bottom 3 strings.",
+    "Use no adjacent strings (Always skip if changing strings).",
+    "Use No string restrictions."];
 
 let secondColumn = [
-    "your pointer finger",
-    "your middle finger",
-    "your ring finger",
-    "your pinky finger",
-    "picking every note",
-    "picking few notes",
-    "slides",
-    "bends",
-    "hammer on's",
-    "open strings",
-    "nothing"]
+    "Focus on your pointer finger",
+    "Focus on your middle finger",
+    "Focus on your ring finger",
+    "Focus on your pinky finger",
+    "Focus on picking every note",
+    "Focus on picking few notes",
+    "Focus on slides",
+    "Focus on bends",
+    "Focus on hammer ons",
+    "Focus on open strings",
+    "Focus on nothing"];
 
 let thirdColumn = [
-    "B or C (Your Choice)",
-    "C#/Db.",
-    "D.",
-    "D#/Eb.",
-    "E.",
-    "F.",
-    "F#/Gb.",
-    "G.",
-    "G#/Ab.",
-    "A.",
-    "A#/Bb."]
+    "In the key of B or C (Your Choice)",
+    "In the key of C#/Db.",
+    "In the key of D.",
+    "In the key of D#/Eb.",
+    "In the key of E.",
+    "In the key of F.",
+    "In the key of F#/Gb.",
+    "In the key of G.",
+    "In the key of G#/Ab.",
+    "In the key of A.",
+    "In the key of A#/Bb."]
 
 function getRandomNumber(max) {
         return Math.floor(Math.random() * max);
     }
 
+/**
+ * The main prompt generator
+ */
+
+generateButton.addEventListener("click", function() {
+    var randomFirstColumn = firstColumn[Math.floor(Math.random() * firstColumn.length)]
+    outputFirst.innerHTML = randomFirstColumn;
+    var randomSecondColumn = secondColumn[Math.floor(Math.random() * secondColumn.length)]
+    outputSecond.innerHTML = randomSecondColumn;
+    var randomThirdColumn = thirdColumn[Math.floor(Math.random() * thirdColumn.length)]
+    outputThird.innerHTML = randomThirdColumn;
+})
 
 
-function generatePrompt() {
-    let num1 = Math.floor(Math.random() * 11);
-    let num2 = Math.floor(Math.random() * 11);
-    let num3 = Math.floor(Math.random() * 11);
-    alert("you clicked the button");
-    }
     
 
 
