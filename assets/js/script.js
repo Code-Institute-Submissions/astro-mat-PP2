ee0b28ba80f4a34385544e01e07eef963ca0d847
astro-mat <whoismatwarren@gmail.com>
2024-01-15 18:04:42 +0000

    debug missing bracket

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,55 +40,88 @@ let thirdColumn = [
     {
     text:"In the key of B.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758180&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-b" title="Improv Launch Pad B" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad B</a></div>',
+    imageMajor: "assets/images/scales/BMajor.jpg",
+    imageMinor: "assets/images/scales/BMelMin.jpg",
+    imagePent: "assets/images/scales/BPenMin.jpg",
     },
     {
     text:"In the key of C.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758162&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-c" title="Improv Launch Pad C" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad C</a></div>',
+    imageMajor: "assets/images/scales/C.jpg",
+    imageMinor: "assets/images/scales/C.jpg",
+    imagePent: "assets/images/scales/C.jpg",
     },
     {
     text:"In the key of C#/Db.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758150&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-db" title="Improv Launch Pad C#/Db" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad C#/Db</a></div>',
+    imageMajor: "assets/images/scales/CsharpMajor.jpg",
+    imageMinor: "assets/images/scales/CsharpMelMin.jpg",
+    imagePent: "assets/images/scales/CsharpPenMin.jpg",
     },
     {
     text:"In the key of D.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758159&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-d" title="Improv Launch Pad D" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad D</a></div>',
+    imageMajor: "assets/images/scales/DMajor.jpg",
+    imageMinor: "assets/images/scales/DMelMin.jpg",
+    imagePent: "assets/images/scales/DPenMin.jpg",
     },
     {
     text:"In the key of D#/Eb.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758138&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-eb" title="Improv Launch Pad D#/Eb" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad D#/Eb</a></div>',
+    imageMajor: "assets/images/scales/DsharpMajor.jpg",
+    imageMinor: "assets/images/scales/DsharpMelMin.jpg",
+    imagePent: "assets/images/scales/DsharpPenMin.jpg",
     },
     {
     text:"In the key of E.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758141&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-e" title="Improv Launch Pad E" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad E</a></div>',
+    imageMajor: "assets/images/scales/EMajor.jpg",
+    imageMinor: "assets/images/scales/EMelMin.jpg",
+    imagePent: "assets/images/scales/EPenMin.jpg",
     },
     {
     text:"In the key of F.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758126&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-f" title="Improv Launch Pad F" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad F</a></div>',
+    imageMajor: "assets/images/scales/FMajor.jpg",
+    imageMinor: "assets/images/scales/FMelMin.jpg",
+    imagePent: "assets/images/scales/FPenMin.jpg",
     },
     {
     text:"In the key of F#/Gb.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758135&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-f-1" title="Improv Launch Pad F#/Gb" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad F#/Gb</a></div>',
-    imageMajor: "assets\images\scales\FsharpMajor.jpg",
-    imageMinor: "assets\images\scales\FsharpMelMin.jpg",
-    imagePent: "assets\images\scales\FsharpPenMin.jpg",
+    imageMajor: "assets/images/scales/FsharpMajor.jpg",
+    imageMinor: "assets/images/scales/FsharpMelMin.jpg",
+    imagePent: "assets/images/scales/FsharpPenMin.jpg",
     },
     {
     text:"In the key of G.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758114&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-g" title="Improv Launch Pad G" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad G</a></div>',
+    imageMajor: "assets/images/scales/GMajor.jpg",
+    imageMinor: "assets/images/scales/GMelMin.jpg",
+    imagePent: "assets/images/scales/GPenMin.jpg",
     },
     {
     text:"In the key of G#/Ab.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758183&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-ab" title="Improv Launch Pad G#/Ab" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad G#/Ab</a></div>',
+    imageMajor: "assets/images/scales/GsharpMajor.jpg",
+    imageMinor: "assets/images/scales/GsharpMelMin.jpg",
+    imagePent: "assets/images/scales/GsharpPenMin.jpg",
     },
     {
     text:"In the key of A.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758186&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-a" title="Improv Launch Pad A" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad A</a></div>',
+    imageMajor: "assets/images/scales/AMajor.jpg",
+    imageMinor: "assets/images/scales/AMelMin.jpg",
+    imagePent: "assets/images/scales/APenMin.jpg",
     },
     {
     text:"In the key of A#/Bb.",
     iframe:'<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788758174&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/oceanographic" title="Oceanographic" target="_blank" style="color: #cccccc; text-decoration: none;">Oceanographic</a> · <a href="https://soundcloud.com/oceanographic/ilp-pad-bb" title="Improv Launch Pad A#/Bb" target="_blank" style="color: #cccccc; text-decoration: none;">Improv Launch Pad A#/Bb</a></div>',
+    imageMajor: "assets/images/scales/AsharpMajor.jpg",
+    imageMinor: "assets/images/scales/AsharpMelMin.jpg",
+    imagePent: "assets/images/scales/AsharpPenMin.jpg",
     }
- 
+]
 /**
  * The main prompt generator
  */
@@ -102,9 +135,9 @@ generateButton.addEventListener("click", function() {
     var randomThirdColumn = thirdColumn[Math.floor(Math.random() * thirdColumn.length)]
     outputThird.innerHTML = randomThirdColumn.text;
     outputIframe.innerHTML = randomThirdColumn.iframe;
-    outputIframe.innerHTML = randomThirdColumn.imageMajor;
-    outputIframe.innerHTML = randomThirdColumn.imageMinor;
-    outputIframe.innerHTML = randomThirdColumn.imagePent;
+    outputMajorScale.innerHTML = randomThirdColumn.imageMajor;
+    outputMinorScale.innerHTML = randomThirdColumn.imageMinor;
+    outputPentScale.innerHTML = randomThirdColumn.imagePent;
 
     
 
