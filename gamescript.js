68f6a7962aa50285919677d6c9384a06e1044286
astro-mat <whoismatwarren@gmail.com>
2024-01-19 15:00:44 +0000

    Add questions in JS file

---
+++ gamescript.js
@@ -0,0 +1,47 @@
+const questions = [
+    {
+        question: Identify this scale,
+        answers: [
+            {text: "C Dorian", correct: false},
+            {text: "C Locrian", correct: false},
+            {text: "C Major", correct: true},
+            {text: "C phrigian", correct: false},
+        ]
+    },
+    {
+        question: Identify this scale,
+        answers: [
+            {text: "C Dorian", correct: true},
+            {text: "C Locrian", correct: false},
+            {text: "C Major", correct: false},
+            {text: "C phrigian", correct: false},
+        ]  
+    },
+    {
+        question: Identify this scale,
+        answers: [
+            {text: "C Dorian", correct: false},
+            {text: "C Locrian", correct: false},
+            {text: "C Major", correct: false},
+            {text: "C phrigian", correct: true},
+        ]
+    },
+    {
+        question: Identify this scale,
+        answers: [
+            {text: "C Dorian", correct: false},
+            {text: "C Locrian", correct: true},
+            {text: "C Major", correct: false},
+            {text: "C phrigian", correct: false},
+        ]
+    },
+    {
+        question: Identify this scale,
+        answers: [
+            {text: "C Dorian", correct: false},
+            {text: "C Locrian", correct: true},
+            {text: "C Major", correct: false},
+            {text: "C phrigian", correct: false},
+        ]
+    }
+]
